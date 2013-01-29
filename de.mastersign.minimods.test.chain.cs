0c6453ce3047532e0aac9a0b6c0b3ba3c4539c54
mastersign <dev@mastersign.de>
2013-01-29 16:41:48 +0100

    added test for chain

---
+++ de.mastersign.minimods.test.chain.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using de.mastersign.minimods.chain;
+
+namespace de.mastersign.minimods.test.chain
+{
+    class ChainTest
+    {
+        [Test]
+        public void IsEmptyTest()
+        {
+            var o = new Chain<int>();
+            Assert.IsTrue(o.IsEmpty);
+        }
+
+        [Test]
+        public void EmptyTest()
+        {
+            var o = Chain<int>.Empty;
+            Assert.NotNull(o);
+            Assert.IsTrue(o.IsEmpty);
+        }
+    }
+}
