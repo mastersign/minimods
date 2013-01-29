4cabb49d26966a9966ed173cd5b7858be0c4e734
mastersign <dev@mastersign.de>
2013-01-29 12:20:55 +0100

    added BitmapToBitmapSource minimod added BitmapTools minimod (incomplete)

---
+++ de.mastersign.minimods.bitmaptobitmapsource.cs
@@ -0,0 +1,90 @@
+﻿/*
+ * MiniMod "Bitmap to BitmapSource"
+ * 
+ * Author: Micheal Eakins
+ * 
+ * Description:
+ *   Conversion from System.Drawing.Bitmap to System.Windows.Media.Imaging.BitmapSource.
+ * 
+ * Version: 1.0
+ * Last Change: 29.01.2013
+ * 
+ * URL: https://raw.github.com/mastersign/minimods/master/de.mastersign.minimods.bitmaptobitmapsource.cs
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Media.Imaging;
+
+namespace de.mastersign.minimods.bitmaptobitmapsource
+{
+    /// <summary>
+    /// <para>
+    /// Contains extension methods for converting a GDI+ <see cref="System.Drawing.Bitmap"/> 
+    /// into a <see cref="BitmapSource"/>.
+    /// </para>
+    /// <para>Author: Micheal Eakins http://stackoverflow.com/users/437301/michael-eakins </para>
+    /// <para>Source: http://stackoverflow.com/questions/94456/load-a-wpf-bitmapimage-from-a-system-drawing-bitmap </para>
+    /// </summary>
+    public static class BitmapToBitmapSourceExtension
+    {
+        /// <summary>
+        /// Converts a <see cref="System.Drawing.Image"/> into a WPF <see cref="BitmapSource"/>.
+        /// </summary>
+        /// <param name="image">The image image.</param>
+        /// <returns>A BitmapSource</returns>
+        public static BitmapSource ToBitmapSource(this System.Drawing.Image image)
+        {
+            using (var bitmap = new System.Drawing.Bitmap(image))
+            {
+                return bitmap.ToBitmapSource();
+            }
+        }
+
+        /// <summary>
+        /// Converts a <see cref="System.Drawing.Bitmap"/> into a WPF <see cref="BitmapSource"/>.
+        /// </summary>
+        /// <remarks>Uses GDI to do the conversion. Hence the call to the marshalled DeleteObject.
+        /// </remarks>
+        /// <param name="bitmap">The bitmap bitmap.</param>
+        /// <returns>A BitmapSource</returns>
+        public static BitmapSource ToBitmapSource(this System.Drawing.Bitmap bitmap)
+        {
+            BitmapSource bitSrc = null;
+
+            var hBitmap = bitmap.GetHbitmap();
+
+            try
+            {
+                bitSrc = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
+                    hBitmap,
+                    IntPtr.Zero,
+                    Int32Rect.Empty,
+                    BitmapSizeOptions.FromEmptyOptions());
+            }
+            catch (Win32Exception)
+            {
+                bitSrc = null;
+            }
+            finally
+            {
+                NativeMethods.DeleteObject(hBitmap);
+            }
+
+            return bitSrc;
+        }
+    }
+
+    internal static class NativeMethods
+    {
+        [DllImport("gdi32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool DeleteObject(IntPtr hObject);
+    }
+}
---
+++ de.mastersign.minimods.bitmaptools.cs
@@ -0,0 +1,77 @@
+﻿/*
+ * MiniMod "Bitmap Tools"
+ * 
+ * Author: Tobias Kiertscher <dev@mastersign.de>
+ * 
+ * Description:
+ *   Bitmap tools provides conversion and access methods for
+ *   reading and writing data in System.Drawing.Bitmap objects.
+ * 
+ * Version: 1.0
+ * Last Change: 29.01.2013
+ * 
+ * URL: https://raw.github.com/mastersign/minimods/master/de.mastersign.minimods.bitmaptools.cs
+ * 
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+
+namespace de.mastersign.minimods.bitmaptools
+{
+    public static class BitmapTools
+    {
+        public static Bitmap CreateFromArray(byte[,] m)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    /// <summary>
+    /// This static class contains a group of extension methods for the
+    /// <see cref="Bitmap"/> class.
+    /// </summary>
+    public static class BitmapExtension
+    {
+        static BitmapExtension()
+        {
+            InitializeGrayScalePalette();
+        }
+
+        #region gray scale palette
+
+        private static ColorPalette paletteGrayScale;
+
+        private static void InitializeGrayScalePalette()
+        {
+            var bmp = new Bitmap(1, 1, PixelFormat.Format8bppIndexed);
+            paletteGrayScale = bmp.Palette;
+            bmp.Dispose();
+            for (var i = 0; i < 256; i++)
+            {
+                paletteGrayScale.Entries[i] = Color.FromArgb(255, i, i, i);
+            }
+        }
+
+        #endregion
+
+        public static byte[,] ToGrayScaleArray(this Bitmap bmp)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static byte[,,] ToRgbArray(this Bitmap bmp)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static byte[,,] ToArgbArray(this Bitmap bmp)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
