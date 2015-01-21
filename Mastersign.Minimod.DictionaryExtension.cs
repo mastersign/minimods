3aef32aeec759392e9f1a0fd686f2b552c0112b3
Tobias Kiertscher <dev@mastersign.de>
2015-01-21 15:18:19 +0100

    added new minimods

---
+++ Mastersign.Minimod.DictionaryExtension.cs
@@ -0,0 +1,76 @@
+﻿#region Minimod
+// <MiniMod>
+//   <Name>Dictionary Extensions</Name>
+//   <Author>Tobias Kiertscher &lt;dev@mastersign.de&gt;</Author>
+//   <LastChanged>2015-01-21</LastChanged>
+//   <Version>1.1.0</Version>
+//   <Url>https://gist.github.com/mastersign/b9065d2cdcdbd13b78db/raw/Mastersign.Minimod.DictionaryExtension.cs</Url>
+//   <Description>
+//     This minimod contains extension methods for the IDictionary<TKey, TValue> interface.
+//   </Description>
+// </MiniMod>
+#endregion
+
+using System;
+using System.Collections.Generic;
+
+namespace Mastersign.Minimod
+{
+    /// <summary>
+    /// This static class contains extension methods for <see cref="IDictionary{TKey,TValue}"/>.
+    /// </summary>
+    public static class DictionaryExtensions
+    {
+        /// <summary>
+        ///     Returns a value from the dictionary,
+        ///     with a default value as fallback in case the given key does not exist.
+        /// </summary>
+        /// <typeparam name="TKey">The type of the key.</typeparam>
+        /// <typeparam name="TValue">The type of the value.</typeparam>
+        /// <param name="dictionary">The dictionary to search in.</param>
+        /// <param name="key">The key to look-up.</param>
+        /// <param name="defaultValue">
+        ///     The optional default value, which is returned in case the <paramref name="key"/> is not found.
+        ///     If no value is given for this parameter, <c>null</c> is used for reference types and 
+        ///     a zero-initialized value is used for value types.
+        /// </param>
+        /// <returns>
+        ///     The value looked-up from the <paramref name="dictionary"/> 
+        ///     or <paramref name="defaultValue"/> in case the <paramref name="key"/> was not found.
+        /// </returns>
+        public static TValue GetValue<TKey, TValue>(this IDictionary<TKey, TValue> dictionary,
+            TKey key, TValue defaultValue = default(TValue))
+        {
+            TValue result;
+            return dictionary.TryGetValue(key, out result)
+                ? result
+                : defaultValue;
+        }
+
+        /// <summary>
+        ///     Returns a value from the dictionary,
+        ///     with a function, generating a default value as fallback, in case the given key does not exist.
+        /// </summary>
+        /// <typeparam name="TKey">The type of the key.</typeparam>
+        /// <typeparam name="TValue">The type of the value.</typeparam>
+        /// <param name="dictionary">The dictionary to search in.</param>
+        /// <param name="key">The key to look-up.</param>
+        /// <param name="defaultGenerator">
+        ///     A call-back funtion which is called in case the key was not found.
+        ///     The return value of the function call is used as the fallback value.
+        /// </param>
+        /// <returns>
+        ///     The value looked-up from the <paramref name="dictionary"/> 
+        ///     or the return value of <paramref name="defaultGenerator"/> 
+        ///     in case the <paramref name="key"/> was not found.
+        /// </returns>
+        public static TValue GetValue<TKey, TValue>(this IDictionary<TKey, TValue> dictionary,
+            TKey key, Func<TValue> defaultGenerator)
+        {
+            TValue result;
+            return dictionary.TryGetValue(key, out result)
+                ? result
+                : defaultGenerator();
+        }
+    }
+}
---
+++ Mastersign.Minimod.TemporaryFolder.cs
@@ -0,0 +1,86 @@
+﻿#region Minimod
+// <MiniMod>
+//   <Name>Temporary Folder</Name>
+//   <Author>Tobias Kiertscher &lt;dev@mastersign.de&gt;</Author>
+//   <LastChanged>2015-01-21</LastChanged>
+//   <Version>1.1.0</Version>
+//   <Url>https://gist.github.com/mastersign/15cf94ebcf356d15da59/raw/Mastersign.Minimod.TemporaryFolder.cs</Url>
+//   <Description>
+//     This minimod contains a class for managing temporary folders in the file system.
+//   </Description>
+// </MiniMod>
+#endregion
+
+using System;
+using System.IO;
+using System.Linq;
+
+namespace Mastersign.Minimod
+{
+    /// <summary>
+    /// Represents a temporary folder in the file system.
+    /// Creates a folder with a unique name at initialization 
+    /// and deletes the folder with all its contents at disposal.
+    /// </summary>
+    public class TemporaryFolder : IDisposable
+    {
+        /// <summary>
+        /// Gets the path of the temporary folder.
+        /// </summary>
+        public string TemporaryPath { get; private set; }
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="TemporaryFolder"/>.
+        /// </summary>
+        /// <param name="basePath">The absolute path of a directory to create the temporary folder in or <c>null</c>.</param>
+        /// <remarks>If <c>null</c> is given for <paramref name="basePath"/>
+        /// <see cref="Path.GetTempPath()"/> is used to retrieve a base path.</remarks>
+        public TemporaryFolder(string basePath = null)
+        {
+            if (basePath == null) basePath = Path.GetTempPath();
+            TemporaryPath = GenerateNonExistingPath(basePath);
+            Directory.CreateDirectory(TemporaryPath);
+        }
+
+        /// <summary>
+        /// Deletes the temporary folder with all its content.
+        /// </summary>
+        public void Dispose()
+        {
+            if (Directory.Exists(TemporaryPath))
+            {
+                Directory.Delete(TemporaryPath, true);
+            }
+        }
+
+        #region Static Helper
+
+        private static readonly Random RAND = new Random();
+
+        private const string PREFIX = "tmp_";
+
+        private static string GenerateRandomName()
+        {
+            var buffer = new byte[4];
+            RAND.NextBytes(buffer);
+            return string.Join(string.Empty, buffer.Select(v => v.ToString("X2")));
+        }
+
+        private static string GeneratePath(string basePath)
+        {
+            return Path.Combine(basePath, PREFIX + GenerateRandomName());
+        }
+
+        private static string GenerateNonExistingPath(string basePath)
+        {
+            string path;
+            do
+            {
+                path = GeneratePath(basePath);
+            } while (Directory.Exists(path));
+            return path;
+        }
+
+        #endregion
+    }
+}
